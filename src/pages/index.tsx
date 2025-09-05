c521cb74b4ea29cee9a5a8acfa78a0e10d13acfa
hackice20 <yashkam431@gmail.com>
2025-09-05 18:26:59 +0530

    changes for deployment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
 import Layout from "../components/layout";
 
 export default function Index() {
-  return <Layout title="Index"></Layout>;
+  return (
+    <Layout title="Index">
+      <p>Welcome to the home page!</p>
+    </Layout>
+  );
 }
